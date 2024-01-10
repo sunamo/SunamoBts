30aeadd1958f6500bfad0af68f0c18f01cc213c8
sunamo <radek.jancik@sunamo.cz>
2024-01-10 11:46:35 +0100

    .

---
+++ _sunamo/CA.cs
@@ -0,0 +1,7 @@
+namespace SunamoBts._sunamo;
+
+internal class CA
+{
+    internal static Func<IList, List<string>> ToListStringIEnumerable2;
+    internal static Func<ChangeContentArgs2, List<string>, Func<string, string>, List<string>> ChangeContent0;
+}
