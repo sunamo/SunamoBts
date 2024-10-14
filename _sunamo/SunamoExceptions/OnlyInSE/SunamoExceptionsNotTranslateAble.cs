66a0d3d17109b0a2e25d8471e663ff0058a74b1a
sunamo.cz <radek.jancik@sunamo.cz>
2024-10-14 16:10:21 +0200

    Removed SunamoExceptions files

--- _sunamo/SunamoExceptions/OnlyInSE/SunamoExceptionsNotTranslateAble.cs
+++
@@ -1,7 +0,0 @@
-namespace SunamoBts._sunamo.SunamoExceptions.OnlyInSE;
-
-internal class SunamoExceptionsNotTranslateAble
-{
-    internal const string NotImplementedCasePublicProgramErrorPleaseContactDeveloper =
-        "Not implemented case. internal program error. Please contact developer";
-}
